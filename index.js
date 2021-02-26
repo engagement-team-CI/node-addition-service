1f45990e597f626a5bd0e30c901082a2bd5e7baa
Roy van de Water <roy.vwater@gmail.com>
2021-02-26 12:37:40 -0700

    Parses query params, adds them

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const add = require('./add')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const a = parseInt(req.query.a)
+  const b = parseInt(req.query.b)
+  res.send(`Hello World!: ${add(a, b)}`)
 })
 
 app.listen(port, () => {
